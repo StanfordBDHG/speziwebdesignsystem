0b958465a2e445b69f7addead12aa1edd2668cda
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-10-21 13:45:48 +0200

    Configure build

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,26 @@
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   root: '.',
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@stanfordbdhg/web-design-system',
+      fileName: (format) => `web-design-system.${format}.js`,
     },
   },
   test: {
