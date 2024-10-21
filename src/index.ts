41b362c20ebc8b93e94836be423b1c4bfec8b282
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-10-21 13:01:26 +0200

    Copy design-system package from ENGAGE-HF

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+export * from './utils/className'
+export * from './utils/tailwind'
+export * from './theme/light'
+export * from './theme/utils'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 //
-// This source file is part of the Stanford Biodesign Digital Health Next.js Template open-source project
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
 //
 // SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
 // SPDX-License-Identifier: MIT
 //
+import tailwindCssAnimate from 'tailwindcss-animate'
+import { tailwindColors } from './src/utils/tailwind'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './{app,pages,components,routes,packages/design-system/src,modules}/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: tailwindColors,
+    },
   },
-  plugins: [],
+  plugins: [tailwindCssAnimate],
 }
